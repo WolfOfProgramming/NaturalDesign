6641f6598c6cec1be09d8b4c6d3b164e2fa137e3
WolfOfProgramming <bartekfc17@gmail.com>
2019-12-01 16:26:03 +0100

    Add Images and Animations

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-const carouselsTracks = document.querySelectorAll('.carousel');
+const carousels = document.querySelectorAll('.carousel');
 const currentElement = [0, 0, 0];
 const dotsNavs = document.querySelectorAll('.carousel__dots-nav')
+const carouselsTracks = document.querySelectorAll('.carousel__tracks-container');
 
-carouselsTracks.forEach( ( carousel, index ) => {
-    const images = carousel.querySelectorAll('.carousel__slide');
-
-    carousel.querySelector('.carousel__btn--left').addEventListener( 'click', (e) => {
-        const elementWidth = images[0].offsetWidth;
-        currentElement[index] === 0 ? currentElement[index] = 2 : currentElement[index] = currentElement[index] - 1;
-        carousel.querySelector('.carousel__tracks-container').style.transform = `translateX(-${elementWidth*currentElement[index]}px)`;
-        updateDots( currentElement, dotsNavs);
-    });
-
-    carousel.querySelector('.carousel__btn--right').addEventListener( 'click', (e) => {
-        const elementWidth = images[0].offsetWidth;
-        currentElement[index] === 2 ? currentElement[index] = 0 : currentElement[index] = currentElement[index] + 1;
-        carousel.querySelector('.carousel__tracks-container').style.transform = `translateX(-${elementWidth*currentElement[index]}px)`;
-        updateDots( currentElement, dotsNavs);
-    });
-});
-
+function changeSlide( currentElement, index, carouselsTracks, elementWidth) {
+    carouselsTracks[index].style.transform = `translateX(-${elementWidth*currentElement[index]}px)`;
+}
 
 function updateDots( currentElement, dotsNavs ) {
     dotsNavs.forEach( (dotsNav, index) => {
@@ -29,12 +15,36 @@ function updateDots( currentElement, dotsNavs ) {
     });
 }
 
+carousels.forEach( ( carousel, index ) => {
+    const images = carousel.querySelectorAll('.carousel__slide');
+
+    carousel.querySelector('.carousel__btn--left').addEventListener( 'click', (e) => {
+        const elementWidth = images[0].offsetWidth;
+        currentElement[index] === 0 ? currentElement[index] = 2 : currentElement[index] = currentElement[index] - 1;
+        // carousel.querySelector('.carousel__tracks-container').style.transform = `translateX(-${elementWidth*currentElement[index]}px)`;
+        changeSlide( currentElement, index, carouselsTracks, elementWidth);
+        updateDots( currentElement, dotsNavs);
+    });
+
+    carousel.querySelector('.carousel__btn--right').addEventListener( 'click', (e) => {
+        const elementWidth = images[0].offsetWidth;
+        currentElement[index] === 2 ? currentElement[index] = 0 : currentElement[index] = currentElement[index] + 1;
+        // carousel.querySelector('.carousel__tracks-container').style.transform = `translateX(-${elementWidth*currentElement[index]}px)`;
+        changeSlide( currentElement, index, carouselsTracks, elementWidth);
+        updateDots( currentElement, dotsNavs);
+    });
+});
+
 dotsNavs.forEach( ( dotsNav, index ) => {
     dotsNav.addEventListener('click', (e) => {
     if( e.target.tagName !== 'BUTTON' ) {
         return;
     }
-    dotsNav.querySelector('.carousel__dot--active').classList.remove('carousel__dot--active');
-    e.target.classList.add('carousel__dot--active');
+    const elementWidth =  document.querySelectorAll('.carousel__slide')[0].offsetWidth;
+    const dots = Array.from(dotsNav.children);
+    const currentDot = dots.findIndex( dot => dot === e.target);
+    currentElement[index] = currentDot; //Sets current carousel element right slide
+    changeSlide( currentElement, index, carouselsTracks, elementWidth);
+    updateDots( currentElement, dotsNavs);
     });
 });
